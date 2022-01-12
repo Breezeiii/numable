27eaa4834b2afe2c91d18bb426c3a5391c03d4c2
YAODAOzzzz <haifeng.liu@ebistrategy.com>
2022-01-12 22:54:15 +0800

    docs

--- vuepress/.vuepress/config.ts
+++ vuepress/.vuepress/config.ts
@@ -1,7 +1,10 @@
 const pkg = require("../../package.json");
 
+const isDev = process.env.NODE_ENV !== "production";
+
 module.exports = {
   // 站点配置
+  base: isDev ? "/" : "./",
   lang: "zh-CN",
   title: `${pkg.name}`,
   description: pkg.description,
